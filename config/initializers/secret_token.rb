1c89effbe6b3b1f71fd676bf98ce7b6b25bf7f13
Derick Hitchcock <derick.hitchcock@scimedsolutions.com>
2012-10-19 09:05:11 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Temptd::Application.config.secret_token = '4eccd0e432b318ef3f560d366e3ecaa70a7ecd95fa45485622fa054530374913cbf230c1c0c343c71d1b574a68157a275f7a20a0ecb35653205d99293858a134'
