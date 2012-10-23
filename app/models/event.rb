dad54d5dd281cd6f8d1ebec44fb42978b5b4d646
Joe Peck <joe.peck@scimedsolutions.com>
2012-10-22 23:03:41 -0400

    "Show" page for events
    Still got a long ways to go

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,16 @@ class Event < ActiveRecord::Base
   has_many        :timeslots
   has_many        :votes
   belongs_to      :event_type
+  
+  validates_presence_of :event_type
+  
+  def to_s
+    "#{event_type}"
+  end
+
+  def starts_at
+    timeslots.order(:starts_at).first.starts_at
+  end
 
   def calculate_event_details
     calculate_event_time
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,4 +2,12 @@ class Timeslot < ActiveRecord::Base
   attr_accessible :ends_at, :event_id, :starts_at
   belongs_to :event
   has_many :votes
+  
+  validates_presence_of :event_id, :starts_at
+  
+  def to_s
+    range = starts_at.strftime("%A:%l%P")
+    range << " - #{ends_at.strftime("%A:%l%P")}" if ends_at
+    return range
+  end
 end
