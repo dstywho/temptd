26914490a59d9998eca1f5c42dd5f652acf6a5e6
Mike Simpson <mike.simpson@scimedsolutions.com>
2012-11-16 23:03:35 -0500

    Event spec runs.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
-before :all do
 
-  vote1 = Vote.new
-  vote2 = Vote.new
-  vote3 = Vote.new
-  timeslot1 = Timeslot.new
-  timeslot2 = Timeslot.new
-  event = Event.new(timeslots: [timeslot1, timeslot2])
-end
 
 describe Event do
+  before :all do
+
+    vote1 = Vote.new
+    vote2 = Vote.new
+    vote3 = Vote.new
+    timeslot1 = Timeslot.new
+    timeslot2 = Timeslot.new
+    event = Event.new(timeslots: [timeslot1, timeslot2])
+  end
+
   it "calculates the most popular time" do
     timeslot1.votes = [vote1, vote2]
     timeslot2.vote = vote3
     event.calculate_event_time.should_be timeslot2
-
   end
 
-end
   it "should calculate_event_time" do
     
   end
