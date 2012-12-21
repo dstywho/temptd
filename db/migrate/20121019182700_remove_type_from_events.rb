a39f456d60e01450d4c770c2d14e89f2b326e2c6
Joe Peck <joe.peck@scimedsolutions.com>
2012-12-21 11:20:46 -0500

    Edit down migration so it fits with the up

--- db/migrate/20121019182700_remove_type_from_events.rb
+++ db/migrate/20121019182700_remove_type_from_events.rb
@@ -2,9 +2,10 @@ class RemoveTypeFromEvents < ActiveRecord::Migration
   def up
     remove_column :events, :type
     remove_column :events, :event_type_id
-    add_column :events, :event_type_id, :integer, after: :id 
+    add_column :events, :event_type_id, :integer, after: :id
   end
 
   def down
+    add_column :events, :type, :integer
   end
 end
