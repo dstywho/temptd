c7e3247ee98029562af3ada21633d3b3f6f82956
Joe Peck <joe.peck@scimedsolutions.com>
2013-01-18 15:35:23 -0500

    Showing event and events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def display_winning_time(event)
+    if event.winning_timeslot
+      event.winning_timeslot.to_s
+    else
+      "None"
+    end
+  end
+  
+  def display_datetime(datetime)
+    datetime.strftime("%A, %l%P")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,28 +18,28 @@ now = Time.now
   sports_event.timeslots.create!(starts_at: starts_at)
 end
 
-sport = EventType.find_or_create_by_name("Sports")
-test_event = Event.find_or_create_by_event_type_id(sport.id)
-test_user = User.create(email: "user@name.com", password: 'password', password_confirmation: 'password', nickname: "The Hammer")
-
-monday = Time.now.beginning_of_week
-
-5.times do |day_index|
-  day = monday + day_index.days
-  2.times do |i|
-    start_time = day + (17 + i * 2).hours 
-    end_time = day + (19 + i * 2).hours 
-
-    Timeslot.create(event_id: test_event.id, starts_at: start_time, ends_at: end_time)
-  end
-end
-
-Vote.create(user_id: test_user.id, event_id: test_event.id, timeslot_id: Timeslot.last.id)
-Vote.create(user_id: 2, event_id: test_event.id, timeslot_id: Timeslot.first.id)
-Vote.create(user_id: 3, event_id: test_event.id, timeslot_id: Timeslot.last.id)
-
-user = User.find_or_initialize_by_email("sports@scimedsolutions.com")
-user.nickname = "Sporty"
-user.password = "sporty"
-user.password_confirmation = "sporty"
-user.save!
+# sport = EventType.find_or_create_by_name("Sports")
+# test_event = Event.find_or_create_by_event_type_id(sport.id)
+# test_user = User.create(email: "user@name.com", password: 'password', password_confirmation: 'password', nickname: "The Hammer")
+# 
+# monday = Time.now.beginning_of_week
+# 
+# 5.times do |day_index|
+#   day = monday + day_index.days
+#   2.times do |i|
+#     start_time = day + (17 + i * 2).hours 
+#     end_time = day + (19 + i * 2).hours 
+# 
+#     Timeslot.create(event_id: test_event.id, starts_at: start_time, ends_at: end_time)
+#   end
+# end
+# 
+# Vote.create(user_id: test_user.id, event_id: test_event.id, timeslot_id: Timeslot.last.id)
+# Vote.create(user_id: 2, event_id: test_event.id, timeslot_id: Timeslot.first.id)
+# Vote.create(user_id: 3, event_id: test_event.id, timeslot_id: Timeslot.last.id)
+# 
+# user = User.find_or_initialize_by_email("sports@scimedsolutions.com")
+# user.nickname = "Sporty"
+# user.password = "sporty"
+# user.password_confirmation = "sporty"
+# user.save!
