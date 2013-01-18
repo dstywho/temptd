dd59e5708a22e9f9190eab3f489ab8be85e664e2
Joe Peck <joe.peck@scimedsolutions.com>
2013-01-18 14:10:14 -0500

    Can log in and log out through Github
    * Also can edit users
    * Also generated devise views to customize them

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
+    
     if user.persisted?
-      sign_in_and_redirect user, notice: "Welcome to TempTD, home of TempTD"
+      flash[:notice] = "Welcome to TempTD, home of TempTD"
+      sign_in_and_redirect user
     else
+      session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :votes
-  validates_presence_of :email
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -8,13 +7,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :nickname, :provider, :uid
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :nickname, :provider, :uid
+
+  def to_s
+    username
+  end
 
   def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
       end
+    else
+      super
     end
   end
   
@@ -26,16 +32,15 @@ class User < ActiveRecord::Base
     end
   end
 
-  # def self.find_for_github_oauth(auth, signed_in_resource=nil)
-  #   user = User.where(:provider => auth.provider, :uid => auth.uid).first
-  #   unless user
-  #     user = User.create(nickname:auth.extra.raw_info.name,
-  #       provider:auth.provider,
-  #       uid:auth.uid,
-  #       email:auth.info.email,
-  #       password:Devise.friendly_token[0,20]
-  #     )
-  #   end
-  #   user
-  # end
+  def password_required?
+    super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 end
