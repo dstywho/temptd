322f6d1ae780955ed3891af3e16aa95abd859582
Joe Peck <joe.peck@scimedsolutions.com>
2013-01-18 17:51:51 -0500

    Get user email from Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid      = auth.uid
+      user.email    = auth.info.email
       user.username = auth.info.nickname
     end
   end
